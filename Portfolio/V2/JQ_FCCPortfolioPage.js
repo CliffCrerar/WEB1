ba83013ae16b0f905cc78da9f1699b499fc03bff
Cliff <cliff.crerar@gmail.com>
2017-05-28 19:15:32 +0200

    Small Update

--- Portfolio/V2/JQ_FCCPortfolioPage.js
+++ Portfolio/V2/JQ_FCCPortfolioPage.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("li").addClass("col-sm-4");
 	$(".navbar-header").css("width", "100%");
-	$("#myCarousel").css("width", "50%")
+	$("#myCarousel").css("width", "100%")
 
 })
