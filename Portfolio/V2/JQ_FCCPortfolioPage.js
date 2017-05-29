78d03dd0597fea59ff110e73f81b594b93b7230c
Cliff <cliff.crerar@gmail.com>
2017-05-29 06:32:23 +0200

    Merged into master branch

--- Portfolio/V2/JQ_FCCPortfolioPage.js
+++ Portfolio/V2/JQ_FCCPortfolioPage.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	$("li").addClass("col-sm-4");
 	$(".navbar-header").css("width", "100%");
 	$("h2").addClass("text-center");
+	$("img").css("width","100%")
+	$("img").css("border-radius","10px")
 	$("img").addClass("img-responsive");
 	$(".item").addClass("image-container")
 	
