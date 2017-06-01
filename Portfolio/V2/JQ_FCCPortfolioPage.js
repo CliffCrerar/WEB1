7399f7a1191017ae7998604276ec175cc6eb9125
Cliff <cliff.crerar@gmail.com>
2017-06-01 10:20:22 +0200

    Completed Portfolio and contact divs

--- Portfolio/V2/JQ_FCCPortfolioPage.js
+++ Portfolio/V2/JQ_FCCPortfolioPage.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
 	$("li").addClass("col-sm-4");
 	$(".navbar-header").css("width", "100%");
 	$("h2").addClass("text-center");
-	$("img").css("width","100%")
-	$("img").css("border-radius","10px")
-	$("img").addClass("img-responsive");
-	$(".item").addClass("image-container")
-	
-
-
+	$("img").css("width","100%");
+	$("img").css("border-radius","20px");
+	$("img").css("img-responsive");	
+	$(".item").addClass("image-container");
+	$(".thumbnail").parent().addClass("col-sm-3","col-xs-12");
 })
