ce8e22ae4d104543acce99386fb45a460257bba0
Cliff <cliff.crerar@gmail.com>
2017-06-03 01:09:40 +0200

    Completed Portfolio Page

--- Portfolio/V2/JQ_FCCPortfolioPage.js
+++ Portfolio/V2/JQ_FCCPortfolioPage.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
-	$("li").addClass("col-sm-4");
+	$(".navbar-nav").children().addClass("col-sm-4");
 	$(".navbar-header").css("width", "100%");
 	$("h2").addClass("text-center");
 	$("img").css("width","100%");
 	$("img").css("border-radius","20px");
 	$("img").css("img-responsive");	
 	$(".item").addClass("image-container");
+	$(".carousel-control").css("border-radius","20px");
 	$(".thumbnail").parent().addClass("col-sm-3","col-xs-12");
+	$("input,textarea").addClass("form-control");
+	$("input,textarea").css("font-size","20px");
 	$("input").prop("placeholder","Type here");
 	$("textarea").prop("placeholder","Please leave a comment")
+	$('[data-toggle="tooltip"]').tooltip();
+
 })
+
