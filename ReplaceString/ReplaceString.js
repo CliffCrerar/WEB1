ebd583aa7f641e9d281b54fef8d7977bb00982a5
Cliff <cliff.crerar@gmail.com>
2017-06-23 08:47:56 +0200

    what?

--- ReplaceString/ReplaceString.js
+++ ReplaceString/ReplaceString.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-	$("#docReady").html("Document Ready")
+	$("#docReady").html("Document Ready");
+	//Paste excel below here
+	var repArr = [
+		['COMPANY_CODE','%V_COM_CD %'],
+		['COMPANY_NAME','%V_COM_NAM %'],
+		['AMOUNT_REPORT_CURRENCY','%V_AMOUNT %'],
+		['REPORT_CURRENCY_INDICATOR','%V_AMT_CUR_IND %'],
+	]
+	
+	
+	//Paste excel above here
+	
+	var path="file:///C:/Users/clifford.crear/Desktop/GL_04_Data_Prep_and_Map_SAP.txt"
+	
+	
+	
+	$("button").click(function(){
+		console.log(repArr[1][0]);
+		console.log(repArr[1][1]);
+		console.log(repArr.length);
+		$.get("C:/Users/clifford.crear/Desktop/GL_04_Data_Prep_and_Map_SAP.txt", function(data){
+			var resource = data;
+			$("#div1").html(data);
+		});
+	});
 });
