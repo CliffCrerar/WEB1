35d727d9c6bf6472cf790d39e8faa5eb95469086
Cliff <cliff.crerar@gmail.com>
2017-07-01 14:49:55 +0200

    Sripted Decryption

--- KeyEncryption/JS_KeyEncryption.js
+++ KeyEncryption/JS_KeyEncryption.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
-	
-	console.log("jQuery working");
-	
-	var sKey = 'b09b0147a1acef7682ebe00f0d4af1d6'
+	//var sKey = 'b09b0147a1acef7682ebe00f0d4af1d6'
 	
 	//Set interface to encrypt this is the default state
 	$("#encrypt").click(function(){
-		console.log("clicked the encrypt button");
 		$("label:first").html("Key to encrypt");
 		$("body").css("background-color","rgb(150, 110, 150)");
 		$("#encryption").html("Encrypt");
@@ -15,21 +11,23 @@ $(document).ready(function(){
 	
 	//Set interface to decrypt
 	$("#decrypt").click(function(){
-		console.log("clicked the decrypt button");
 		$("label:first").html("Key to decrypt");		
 		$("body").css("background-color","rgb(100, 150, 100)");
 		$("#encryption").html("Decrypt");
 		$("label:last").html("Decrypted key");		
 	});
 	
+	//Encrypt or decrypt strings
 	$("#encryption").click(function(){
-		console.log("click1");
-		//Change the style of the button
 		$("#encryption").removeClass("btn-warning");
-		$("#encryption").addClass("btn-success");
-				
-		console.log(sKey);
-		$("#keyOut").val(encrypt($("#keyInput").val()));		
+		$("#encryption").addClass("btn-success");	
+		
+		if ($("#encryption").html() == "Encrypt") {
+			$("#keyOut").val(encrypt($("#keyInput").val()));
+		}
+		else {
+			$("#keyOut").val(decrypt($("#keyInput").val()));
+		}
 	});	
 	
 	//Encryption function
@@ -39,27 +37,30 @@ $(document).ready(function(){
 		var nPos = 0;
 		var nLetter = '';
 		var nKey = ''
-		
-		console.log(keyLen);
-				
+						
 		for (i = 0; i < keyLen; i++) {
 			oLetter = key[i];
 			nPos = key.codePointAt(i)+13;
 			nLetter = String.fromCharCode(nPos)
-			nKey = nKey + nLetter;
-			
-			console.log("i = "+i);
-			console.log("Old Letter = "+oLetter);
-			console.log("New UNICODE letter number = "+nPos);
-			console.log("New UNICODE letter = "+nLetter);
-			console.log("New Key = "+nKey);
 		}
 		return nKey
 	}
 	
 	//Dycryption function
 	function decrypt(key) {
-		
+		var keyLen = key.length;
+		var oLetter = '';
+		var nPos = 0;
+		var nLetter = '';
+		var nKey = ''
+						
+		for (i = 0; i < keyLen; i++) {
+			oLetter = key[i];
+			nPos = key.codePointAt(i)-13;
+			nLetter = String.fromCharCode(nPos)
+			nKey = nKey + nLetter;
+		}
+		return nKey
 	}
 
 });
--- WeatherPage/JS_weatherPage.js
+++ WeatherPage/JS_weatherPage.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 			$("#lat").html(lat1);
 			$("#long").html(long1);
 			var fLink = "https://api.darksky.net/forecast/";
-			var secretKey = "b09b0147a1acef7682ebe00f0d4af1d6";
+			var secretKey = decrypt("b09b0147a1acef7682ebe00f0d4af1d6");
 			apiLink = fLink+secretKey+"/"+lat1+","+long1;
 			//$("#link").html(apiLink);
 			$("#dsApi").attr("src",apiLink);
@@ -19,18 +19,27 @@ $(document).ready(function(){
 	var invocation = new XMLHttpRequest();
 	var url = apiLink
 	
-	function callOtherDomain(){
-		if (invocation) {
-			invocation.open("GET",url,true);
-			invocation.onreadystatechange = handler;
-			invocation.send();
-		}
-	}
-		
 	$("#change").on("click", function(){
 		$.getJSON(apiLink,function(forecast){
 			console.log(forecast);
 		});
 	});
+	
+	//Dycryption function
+	function decrypt(key) {
+		var keyLen = key.length;
+		var oLetter = '';
+		var nPos = 0;
+		var nLetter = '';
+		var nKey = ''
+						
+		for (i = 0; i < keyLen; i++) {
+			oLetter = key[i];
+			nPos = key.codePointAt(i)-13;
+			nLetter = String.fromCharCode(nPos)
+			nKey = nKey + nLetter;
+		}
+		return nKey
+	}
 	
 });
