f567a23aa8ab188d321c6cdc8799996e13335948
Cliff Crerar <cliff.crerar@gmail.com>
2017-07-02 11:31:52 +0200

    Weather Page Take 2

---
+++ GeoLocation
@@ -0,0 +1,5 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
--- WeatherPage/JS_weatherPage.js
+++ WeatherPage/JS_weatherPage.js
@@ -1,3 +1,45 @@
 $(document).ready(function(){
     console.log("JQ workings")
+
+    var posLat;
+    var posLong;
+    var sUrl = 'https://api.darksky.net/forecast/';
+    var encKey = 'o=Fo=>ADn>nprsDCE?ror==s=qAns>qC';
+    var apiLink = '';
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            posLat = position.coords.latitude;
+            posLong = position.coords.longitude;
+            
+            $("#lat").html(posLat);
+            $("#long").html(posLong);
+            
+            apiLink = sUrl+decrypt(encKey)+'/'+posLat+','+posLong
+
+            $("#encLink").html(apiLink);
+
+            console.log(posLat);
+            console.log(posLong);
+        });
+    }
+
+    //Dycryption function
+	function decrypt(key) {
+		var keyLen = key.length;
+		var oLetter = '';
+		var nPos = 0;
+		var nLetter = '';
+		var nKey = ''
+						
+		for (i = 0; i < keyLen; i++) {
+			oLetter = key[i];
+			nPos = key.codePointAt(i)-13;
+			nLetter = String.fromCharCode(nPos)
+			nKey = nKey + nLetter;
+		}
+		return nKey
+	}
+
+
 });
